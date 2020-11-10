531544afdfd3c226a99fa86f28cfbd044822242e
Karl Beaumont <karl.beaumont@firstmac.com.au>
2020-11-11 07:21:24 +0800

    Integrated firebase in training service.

--- fitness-tracker/src/app/training/training.service.ts
+++ fitness-tracker/src/app/training/training.service.ts
@@ -1,26 +1,40 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { map } from 'rxjs/operators/map';
 import { Subject } from 'rxjs/Subject';
 import { Exercise } from './exercise.model';
 
+@Injectable()
 export class TrainingService {
     exerciseChanged = new Subject<Exercise>();
-
-    private availableExercises: Exercise[] = [
-        { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
-        { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
-        { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
-        { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
-    ];
-
+    exercisesChanged = new Subject<Exercise[]>();
+    private availableExercises: Exercise[] = [];
     private runningExercise: Exercise;
     private exercises: Exercise[] = [];
 
-    getAvailableExercises() {
-        return [...this.availableExercises];
+    constructor(private db: AngularFirestore) {}
+
+    fetcAvailableExercises() {
+        this.db.collection('availableExercises')
+        .snapshotChanges()
+        .pipe(map(docArray => {
+          return docArray.map(doc => {
+            const document: any = doc;
+            return {
+              id: document.payload.doc.id,
+              ...document.payload.doc.data(),
+            }
+          })
+        }))
+        .subscribe((exercises: Exercise[]) => {
+            console.log(exercises);
+            this.availableExercises = exercises;
+            this.exercisesChanged.next([...this.availableExercises])
+        })
     }
 
     startExercise(selectedId: string) {
-        this.runningExercise = this.availableExercises.find(exercise => exercise.id === selectedId);
-        console.log(selectedId, this.runningExercise)
+        this.runningExercise = this.availableExercises.find(exercise => exercise.name === selectedId);
         this.exerciseChanged.next({ ...this.runningExercise });
     }
 
