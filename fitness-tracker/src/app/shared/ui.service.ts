0b0ae50f52d81ebf1014a852aeaf5a81bae85ba8
Karl Beaumont <karl.beaumont@firstmac.com.au>
2020-11-12 12:21:05 +0800

    Added ui service.

---
+++ fitness-tracker/src/app/shared/ui.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subject } from 'rxjs';
+
+@Injectable()
+export class UIService {
+    loadingStateChanged = new Subject<boolean>();
+
+    constructor(private snackBar: MatSnackBar) {}
+
+    showSnackBar(message, action, duration) {
+        this.snackBar.open(message, action, {
+            duration,
+        })
+    }
+}
